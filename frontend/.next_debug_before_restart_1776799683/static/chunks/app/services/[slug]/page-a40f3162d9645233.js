28ae7cab28be46894e7c545cd12d9435571c5259
sb391 <sb391@users.noreply.github.com>
2026-04-22 13:22:40 +0530

    Remove debug build artifacts from repo

--- frontend/.next_debug_before_restart_1776799683/static/chunks/app/services/[slug]/page-a40f3162d9645233.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[805],{9118:(e,s,n)=>{Promise.resolve().then(n.bind(n,6455)),Promise.resolve().then(n.bind(n,5997)),Promise.resolve().then(n.bind(n,6606)),Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[619,958,590,968,441,255,358],()=>e(e.s=9118)),_N_E=e.O()}]);
